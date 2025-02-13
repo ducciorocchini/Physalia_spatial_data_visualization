4c4972d7315a01fb94374b7bd658f6446aa197e4
Duccio Rocchini <ducciorocchini@gmail.com>
2025-02-13 13:15:03 +0100

    Create colorist.R

---
+++ Code/colorist.R
@@ -0,0 +1,19 @@
+# https://besjournals.onlinelibrary.wiley.com/doi/epdf/10.1111/2041-210X.13477
+
+# First map
+
+library(colorist)
+data("fiespa_occ")
+
+# First map with single distributions
+m1 <- metrics_pull(fiespa_occ)
+p1 <- palette_timecycle(fiespa_occ)
+map_multiples(m1, p1, ncol=4, labels=names(fiespa_occ))
+
+# Common map
+m1_distill <- metrics_distill(fiespa_occ)
+map_single(m1_distill, p1)
+
+# Spheric graph
+legend_timecycle(p1, origin_label="Jan 1")
+
