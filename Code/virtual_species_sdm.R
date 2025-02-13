95a2328011983707b8dfc66cfa9b8dfe2ef8160c
Duccio Rocchini <ducciorocchini@gmail.com>
2025-02-13 13:28:19 +0100

    Update virtual_species_sdm.R

--- Code/virtual_species_sdm.R
+++ Code/virtual_species_sdm.R
@@ -1,1 +1,2 @@
-# https://github.com/b-cubed-eu/biospace25-demo/blob/main/README_Rocio.md
+# rocio: https://github.com/b-cubed-eu/biospace25-demo/blob/main/README_Rocio.md
+# paper: https://www.ecography.org/sites/ecography.org/files/appendix/ecog-01388.pdf
