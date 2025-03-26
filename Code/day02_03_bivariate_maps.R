bb188bbfef0a67b07b449e1e72a1e319b5883c13
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-26 16:51:52 +0100

    Create day02_03_bivariate_maps.R

---
+++ Code/day02_03_bivariate_maps.R
@@ -0,0 +1,33 @@
+# https://cran.r-project.org/web/packages/biscale/vignettes/biscale.html
+
+# install.packages("biscale")
+# install.packages(c("cowplot", "sf"))
+
+library(biscale)
+library(ggplot2)
+library(cowplot)  # for the ggdraw function
+
+# create classes
+data <- bi_class(stl_race_income, x = pctWhite, y = medInc, style = "quantile", dim = 3)
+
+# create map
+map <- ggplot() +
+  geom_sf(data = data, mapping = aes(fill = bi_class), color = "white", size = 0.1, show.legend = FALSE) +
+  bi_scale_fill(pal = "GrPink", dim = 3) +
+  labs(
+    title = "Race and Income in St. Louis, MO",
+    subtitle = "Gray Pink (GrPink) Palette"
+  ) +
+  bi_theme()
+
+# legend
+legend <- bi_legend(pal = "GrPink",
+                    dim = 3,
+                    xlab = "Higher % White ",
+                    ylab = "Higher Income ",
+                    size = 8)
+
+# combine map with legend
+finalPlot <- ggdraw() +
+  draw_plot(map, 0, 0, 1, 1) +
+  draw_plot(legend, 0.2, .65, 0.2, 0.2)
